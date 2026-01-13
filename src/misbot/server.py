039a5044686e3bf62d048e60546ee3be9a01fb69
Bohdan Stratila <googlobody@gmail.com>
2026-01-13 02:12:40 +0100

    Webhook code update

--- src/misbot/server.py
+++ src/misbot/server.py
@@ -4,7 +4,11 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 ENVIRONMENT = os.environ.get("ENVIRONMENT")
-TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+WEBHOOK_SECRET_TOKEN = os.environ.get("WEBHOOK_SECRET_TOKEN")
+SSL_KEY_FILE_PATH = os.environ.get("SSL_KEY_FILE")
+SSL_CERT_FILE_PATH = os.environ.get("SSL_CERT_FILE_PATH")
+WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
 
 
 logging.basicConfig(
@@ -19,7 +23,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 if __name__ == "__main__":
-    application = ApplicationBuilder().token(TOKEN).build()
+    application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
     start_handler = CommandHandler("start", start)
     application.add_handler(start_handler)
@@ -28,10 +32,10 @@ if __name__ == "__main__":
         application.run_webhook(
             listen="0.0.0.0",
             port=8443,
-            secret_token="ASecretTokenIHaveChangedByNow",
-            key="private.key",
-            cert="cert.pem",
-            webhook_url="https://example.com:8443",
+            secret_token=WEBHOOK_SECRET_TOKEN,
+            key=SSL_KEY_FILE_PATH,
+            cert=SSL_CERT_FILE_PATH,
+            webhook_url=WEBHOOK_URL,
         )
     else:
         application.run_polling()
