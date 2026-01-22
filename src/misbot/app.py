2f45b335ff38a0225fc2b6deff18f03b3a9b0563
Bohdan Stratila <googlobody@gmail.com>
2026-01-22 23:46:39 +0100

    Support webhook

--- src/misbot/app.py
+++ src/misbot/app.py
@@ -1,7 +1,12 @@
 import asyncio
 import logging
 
+from telegram import Update
+from telegram.ext import Application
+from uvicorn import Server
+
 from misbot.bot.app import get_bot_app
+from misbot.config import ENVIRONMENT, WEBHOOK_SECRET_TOKEN, WEBHOOK_URL
 from misbot.database.db import engine
 from misbot.server import init_uvicorn_server
 from misbot.server.app import fastapi_app
@@ -11,19 +16,36 @@ logging.basicConfig(
 )
 
 
+async def run_polling(bot_app: Application, server: Server):
+    async with bot_app.updater:
+        await bot_app.updater.start_polling()
+        await server.serve()
+        await bot_app.updater.stop()
+
+
+async def setup_webhook(bot_app: Application):
+    bot_app.updater = None
+    await bot_app.bot.set_webhook(
+        url=WEBHOOK_URL,
+        allowed_updates=Update.ALL_TYPES,
+        secret_token=WEBHOOK_SECRET_TOKEN,
+    )
+
+
 async def main():
-    bot = get_bot_app()
+    bot_app = get_bot_app()
     server = init_uvicorn_server(app=fastapi_app)
 
-    async with bot:
-        await bot.start()
+    async with bot_app:
+        await bot_app.start()
 
-        async with bot.updater:
-            await bot.updater.start_polling()
+        if ENVIRONMENT == "prod":
+            await setup_webhook(bot_app)
             await server.serve()
-            await bot.updater.stop()
+        else:
+            await run_polling(bot_app, server)
 
-        await bot.stop()
+        await bot_app.stop()
 
 
 if __name__ == "__main__":
--- src/misbot/server/app.py
+++ src/misbot/server/app.py
@@ -1,9 +1,14 @@
 import logging
 from contextlib import asynccontextmanager
+from http import HTTPStatus
+from typing import Annotated
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Header, HTTPException, Request
+from telegram import Update
+from telegram.ext import Application
 
 from misbot.bot.app import get_bot_app
+from misbot.config import WEBHOOK_SECRET_TOKEN
 from misbot.database import exec as db
 from misbot.server.schemas import PlayerJoinPostRequestBody
 
@@ -21,6 +26,27 @@ fastapi_app = FastAPI(lifespan=lifespan)
 
 @fastapi_app.get("/")
 def tatus():
+    return {"status": "ok"}
+
+
+@fastapi_app.post("/webhook")
+async def webhook(
+    request: Request,
+    x_telegram_bot_api_secret_token: Annotated[str | None, Header()] = None,
+):
+    if not x_telegram_bot_api_secret_token:
+        return HTTPException(status_code=HTTPStatus.UNAUTHORIZED)
+
+    if x_telegram_bot_api_secret_token != WEBHOOK_SECRET_TOKEN:
+        logger.info(
+            "X-Telegram-Bot-Api-Secret-Token header's value is wrong or missing."
+        )
+        raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED)
+
+    bot_app: Application = request.app.state.bot_app
+    await bot_app.update_queue.put(
+        Update.de_json(data=await request.json(), bot=bot_app.bot)
+    )
     return {"status": "ok"}
 
 
