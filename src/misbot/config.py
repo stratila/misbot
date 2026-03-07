598292af62f7ee1633c48ce0794f9209dad710d6
Bohdan Stratila <googlobody@gmail.com>
2026-03-07 22:43:53 +0100

    bool for sqlecho

--- src/misbot/config.py
+++ src/misbot/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 
 ENVIRONMENT = os.environ.get("ENVIRONMENT")
-SQL_ECHO = bool(os.environ.get("SQL_ECHO", 0))
+SQL_ECHO = bool(int(os.environ.get("SQL_ECHO", 0)))
 TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 WEBHOOK_SECRET_TOKEN = os.environ.get("WEBHOOK_SECRET_TOKEN")
 URL_PATH = os.environ.get("URL_PATH")
