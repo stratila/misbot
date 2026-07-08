42983f20525bb1db71b0b6bfd161a00067620137
Bohdan Stratila <googlobody@gmail.com>
2026-07-08 21:19:57 +0200

    Experimenting with workflow calls

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 set -e
 
+if [ "$#" -gt 0 ]; then
+  echo "Executing override command..."
+  exec "$@"
+fi
+
 echo "Running database migrations..."
 /opt/venv/bin/alembic -c /app/alembic.ini upgrade head
 
